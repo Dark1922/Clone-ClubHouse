808aeaacc30ae7c1c64a72cef45e3e0e63a81afc
dark1922 <jvdematos004@gmail.com>
2021-05-18 22:28:49 -0300

    socket

--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,5 +1,24 @@
 import { constants } from "../../_shared/constants.js"
+import RoomController from "./controller.js"
 import RoomSocketBuilder from "./util/roomSocket.js"
+import View from "./view.js"
+
+
+const urlParams = new URLSearchParams(window.location.search)
+const keys = ['id', 'topic']
+const urlData = keys.map((key) => [key, urlParams.get(key)])
+
+
+
+const user = {
+    img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
+    username: 'Erick ' + Date.now()
+}
+
+const roomInfo = {
+    room: { ...Object.fromEntries(urlData) },
+    user
+}
 
 
 const socketBuilder = new RoomSocketBuilder({
@@ -7,20 +26,13 @@ const socketBuilder = new RoomSocketBuilder({
     namespace: constants.socketNamespaces.room
 })
 
-const socket = socketBuilder
-    .setOnUserConnected((user) => console.log('user connected!', user))
-    .setOnUserDisconnected((user) => console.log('user disconnected!', user))
-    .setOnRoomUpdated((room) => console.log('room list!', room))
-    .build()
-
-const room = {
-    id: '0001',
-    topic: 'JS Expert éh noix'
+const dependencies = {
+    view: View,
+    socketBuilder,
+    roomInfo
 }
 
-const user = {
-    img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
-    username: 'Erick ' + Date.now()
-}
 
-socket.emit(constants.events.JOIN_ROOM, { user, room })
+await RoomController.initialize(dependencies)
+
+
--- app/pages/room/src/util/roomSocket.js
+++ app/pages/room/src/util/roomSocket.js
@@ -6,6 +6,7 @@ export default class RoomSocketBuilder extends SocketBuilder {
     constructor({ socketUrl, namespace }) {
         super({ socketUrl, namespace })
         this.onRoomUpdated = () => { }
+        this.onUserProfileUpgrade = () => { }
     }
 
     setOnRoomUpdated(fn) {
@@ -14,10 +15,17 @@ export default class RoomSocketBuilder extends SocketBuilder {
         return this
     }
 
+    setOnUserProfileUpgrade(fn) {
+        this.onUserProfileUpgrade = fn
+
+        return this
+    }
+
     build() {
         const socket = super.build()
 
         socket.on(constants.events.LOBBY_UPDATED, this.onRoomUpdated)
+        socket.on(constants.events.UPGRADE_USER_PERMISSION, this.onUserProfileUpgrade)
 
         return socket;
     }
